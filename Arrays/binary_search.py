b75d2ea47e86356da7279b4bdcc1173ec923a609
Luiz Roberto Pereira Scolari <luizrobertoscolari@outlook.com>
2025-02-28 16:08:14 -0400

    Correct implementation of binary search

--- Arrays/binary_search.py
+++ Arrays/binary_search.py
@@ -1,17 +1,19 @@
-## FIRST IMPLEMENTATION
 def binary_search(array, n):
-    t = len(array)//2 - 1
-    r = 0
+    t, r = len(array)-1, 0
     steps = 0
-    while n != array[t]:
-        if n < array[t]:
-            dif = (t - r)//2 + 1
-            t = int(t - dif)
-        else:
-            dif = (t - r)//2 + 1
-            r = t
-            t = t + dif
+    while r <= t:
+        mid = (t+r) // 2
         steps += 1
-    print(steps+1)
 
-binary_search([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 17)
+        if array[mid] == n:
+            print(steps)
+            return
+        elif array[mid] < n:
+            r = mid + 1
+        else: 
+            t = mid
+    print("-1")
+
+
+binary_search([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 5)
+binary_search([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40], 5)
